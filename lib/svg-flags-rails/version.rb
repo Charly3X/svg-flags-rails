7b0aa562d1220ed25f26672a0c32d81df74bfe3f
charoyan <kinovhod@gmail.com>
2016-10-18 09:18:20 +0300

    newflag

--- lib/svg-flags-rails/version.rb
+++ lib/svg-flags-rails/version.rb
@@ -1,4 +1,4 @@
 module SvgFlagsRails
-  VERSION = '0.1.0' # https://github.com/koppi/iso-country-flags-svg-collection@285c21fb080c3ee132f494afa4f79741903b8c6c
+  VERSION = '0.1.1' # https://github.com/koppi/iso-country-flags-svg-collection@285c21fb080c3ee132f494afa4f79741903b8c6c
 end
 
